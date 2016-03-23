2374dcb685e8f48c02f216b852d95711496dcf32
MariyaS <mariya.sadakova@gmail.com>
2016-03-23 20:38:23 +0100

    Added positiong of accelerating body.
    Can improve formatting later.

--- test.py
+++ test.py
@@ -31,6 +31,10 @@ def convert_ft_to_meters(feet):
     # 1 inch = 2.54 cm = 0.0254m
     return feet*12*0.0254
 
+""" Position of accelerating body. """
+def position_of_accelerating_body(a, v0, t, p0):
+    return (0.5)*a*(t**2) + v0*t + p0
+
 def test_run():
     """ A ball dropped from a tower. Give height and seconds. """
     drop_from_tower_n1()
@@ -42,6 +46,12 @@ def test_run():
     feet = 10
     print "10 feet in meters: "
     print convert_ft_to_meters(feet)
+
+    """ Position of a body given acceleration (a), initial velocity (v0), time (t), initial position (p0) """
+    """ Formula in meters and seconds: p(t) = (1/2)at^2 + v0(t) + p0 """
+    """ In the example below, a=2 m/s^2, v0=2 m/s, t = 2 s, p0 = 0 mz"""
+    print "The position in meters of a body accelerating at 2m/s^2, intial velocity 2m/s, time 2 s, initial position 0 meters: "
+    print position_of_accelerating_body(2, 2, 2, 0)
 
 if __name__ == '__main__':
     test_run()
